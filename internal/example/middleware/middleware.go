2b1dfde15cb6739555b8c73e807790bb9042828b
Dean Milojevic <milojevicdean@gmail.com>
2020-03-06 21:41:19 +0100

    Experimenting with anonymous struct

--- internal/example/middleware/middleware.go
+++ internal/example/middleware/middleware.go
@@ -7,10 +7,22 @@ import (
 )
 
 func Get(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("ContentType", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	response := models.Response {
 		Message: "test message",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetAnon(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response := struct {
+		Message string
+	}{
+		Message: "test with anonymous struct",
 	}
 
 	json.NewEncoder(w).Encode(response)
--- internal/example/router/router.go
+++ internal/example/router/router.go
@@ -10,7 +10,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/example", middleware.Get).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/example", middleware.GetAnon).Methods("GET", "OPTIONS")
 
 	return router
 }
